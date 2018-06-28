2f8117c6994f6ab6e0f92d43ee7cf4f8428ef32b
jjahn0 <35445039+jjahn0@users.noreply.github.com>
2018-06-27 18:24:54 -0700

    update table

--- static/js/table.js
+++ static/js/table.js
@@ -10,12 +10,13 @@
 
 var dataTable = $.get('/table');
 
-dataTable.done( function(results){
+dataTable.done( function(data){
+    results = data.slice(0,9);
     const keys = Object.keys(results[0]);
-    console.log(keys);
+    // console.log(keys);
     var table = d3.select("#datatable")
         .append("table")
-        .attr("class", "table table-striped");
+        .attr("class", "table table-striped table-responsive");
     var thead = table.append("thead");
     var tbody = table.append("tbody");
 
@@ -38,6 +39,9 @@ dataTable.done( function(results){
         })
         .enter()
         .append('td')
+            .attr("style", "font-size:7px")
+            // .append('a')
+            // .attr('href', '#')
             .text(function(d){return d.value});
 
 })
